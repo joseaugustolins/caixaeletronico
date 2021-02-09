a8d838351dff579e5c20ec5fdf07b6adc270a893
Jose Augusto <joseaugustolins@gmail.com>
2021-02-09 00:44:02 -0300

    enable cors

--- src/main/java/com/test/caixaeletronico/CaixaeletronicoApplication.java
+++ src/main/java/com/test/caixaeletronico/CaixaeletronicoApplication.java
@@ -4,12 +4,23 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CaixaeletronicoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CaixaeletronicoApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("*");
+			}
+		};
 	}
 
 	@Bean
